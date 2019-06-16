fa46c334c8f1ba6b7e7392e063f25f64931baea9
Vasilii <kiriushinvasilii@gmail.com>
2019-06-16 17:33:03 +0300

    Task started 01

--- src/javase01/task05/Main.java
+++ src/javase01/task05/Main.java
@@ -3,5 +3,6 @@ package javase01.task05;
 public class Main {
     public static void main(String[] args) {
         System.out.println("skibidi");
+        System.out.println("Blah blah blah");
     }
 }
