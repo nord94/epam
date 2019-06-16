fa5e7009d5d600f63898859535dd21b84b0abe83
Vasilii <kiriushinvasilii@gmail.com>
2019-06-16 18:18:19 +0300

    task05 done

--- src/javase01/task05/Main.java
+++ src/javase01/task05/Main.java
@@ -1,8 +1,17 @@
 package javase01.task05;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("skibidi");
-        System.out.println("Blah blah blah");
+        Scanner input = new Scanner(System.in);
+        int size = 0;
+        size = input.nextInt();
+        for (int i =0;i<size;i++){
+            for (int j = 0;j<size;j++){
+                System.out.print(i==j | i+j==size-1 ? "1 ":"0 ");
+                if (j==size-1) System.out.print("\n");
+            }
+        }
     }
 }
