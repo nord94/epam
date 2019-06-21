15847e49474549e66587a66499e6e3c164255311
Vasilii <kiriushinvasilii@gmail.com>
2019-06-21 15:01:06 +0300

    Wrong name input

--- src/javase02/task02/Worker.java
+++ src/javase02/task02/Worker.java
@@ -7,6 +7,7 @@ public class Worker {
     Chancellery[] chancelleries;
     int summ;
     static Scanner input = new Scanner(System.in);
+    static Scanner inputNames = new Scanner(System.in);
 
     private void calculatePriceOfChancellery() {
         int summ = 0;
@@ -45,11 +46,21 @@ public class Worker {
     }
 
     public static void main(String[] args) {
-        System.out.println("Enter name -> press 'Enter' -> enter number of chancellery");
-        String name = input.nextLine();
-        int numOfChancellery = input.nextInt();
-        Worker worker = new Worker(name, numOfChancellery);
-        worker.collectChancellery();
-        System.out.println(worker.summ);
+        int numOfWorkers;
+        System.out.println("Enter number of workers");
+        numOfWorkers = input.nextInt();
+        Worker[] workers = new Worker[numOfWorkers];
+        for (int k = 0; k < workers.length; k++){
+            System.out.println("Enter number of chancellery -> press 'Enter' -> Enter name");
+            int numOfChancellery;
+            numOfChancellery = input.nextInt();
+            //String name = inputNames.nextLine();
+            String name = input.nextLine(); //If ill use this, java will throw InputMismatchException
+            workers[k]= new Worker(name,numOfChancellery);
+            workers[k].collectChancellery();
+            System.out.println(workers[k].summ);
+        }
+
+
     }
 }
